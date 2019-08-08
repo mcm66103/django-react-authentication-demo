1981997bee8c0b2250ef5c8102c26ac754c3b3db
Maverick <me@maverickmoore.com>
2019-08-08 12:52:33 -0400

    router with loggedin/loggedout paths

--- server/server/decorators.py
+++ server/server/decorators.py
@@ -1,18 +1,30 @@
 import jwt
 
-from django.core.exceptions import PermissionDenied
+from django.http import HttpResponseForbidden
 from django.contrib.auth import authenticate
 
 def requires_authoriaztion(function):
     def wrap(request, *args, **kwargs):
         authorization = request.headers.get('authorization')
-        print(authorization)
-        data = jwt.decode(authorization.split(' ')[1], 'secret', algorithms=['HS256'])
-        user = authenticate(username=data['user'], password=data['password'])
-        if True:
+
+        user = decodeJwtAndReturnUser(authorization)
+        if user:
             return function(request, *args, **kwargs)
+
         else:
-            raise PermissionDenied
+            return HttpResponseForbidden()
+
     wrap.__doc__ = function.__doc__
     wrap.__name__ = function.__name__
     return wrap
+
+
+def decodeJwtAndReturnUser(authorization_header):
+    try:
+        data = jwt.decode(authorization_header.split(' ')[1], 'secret', algorithms=['HS256'])
+        user = authenticate(username=data['user'], password=data['password'])
+        return user
+
+    except Exception as e:
+        print(e)
+        return None
